0b744bd1d231d6f41f53589747c011b3bb099b06
Antony JACQUES <antony.jacques-ext@gl-events.com>
2023-12-20 08:19:43 +0100

    connecting to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import firestore from "firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDWUgLAEq39fHvRSo2wUladS5qdWa2Ez0",
+  authDomain: "vue-playlists-d13a7.firebaseapp.com",
+  projectId: "vue-playlists-d13a7",
+  storageBucket: "vue-playlists-d13a7.appspot.com",
+  messagingSenderId: "63543940593",
+  appId: "1:63543940593:web:b74583be3cfff9c3706050",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+//timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
