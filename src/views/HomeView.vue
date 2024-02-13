c7f61f3e104cb535e718c4b7f9bd683b37a6d369
Antony JACQUES <antony.jacques-ext@gl-events.com>
2024-02-13 22:03:52 +0100

    display real-time playlist data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="home">
-  <p>homepage</p>
+  <div v-if="error" class="error"> Could not fetch the data</div>
+  <div v-if="documents">
+    <div v-for="playlist in documents" :key="playlist.id"> {{ playlist.title }}</div>
+  </div>
+  <p v-else> {{ error }}</p>
   </div>
 </template>
 
 <script>
+import getCollection from '@/composables/getCollection';
 
 export default {
   name: 'HomeView',
   components: {
     
+  },
+  setup(){
+    const { documents, error } = getCollection('playlists');
+
+    return { documents, error }
   }
 }
 </script>
